d42cc4972e61a5491ae61015ad943f7ae48147de
RENAN SCHEIDT RESCHKE <19.02009-0@maua.br>
2021-07-10 15:30:37 -0300

    classe Aula

---
+++ src/models/aula.py
@@ -0,0 +1,14 @@
+from src.enum.sala import Sala
+
+from src.models.professor import Professor
+from src.models.disciplina import Disciplina
+
+from pydantic import BaseModel
+from datetime import datetime, time
+
+class Aula(BaseModel):
+    disciplina: Disciplina
+    local: Sala
+    horario: datetime
+    duracao: time
+    professor: Professor
