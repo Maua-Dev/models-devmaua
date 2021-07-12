a119fa9fa169b6108a47b37ecf413d1d2227dc21
RENAN SCHEIDT RESCHKE <19.02009-0@maua.br>
2021-07-12 14:23:22 -0300

    test endereco; validators

--- src/models/endereco.py
+++ src/models/endereco.py
@@ -1,10 +1,29 @@
-from src.enum.tipo_endereco import TipoEndereco
-from pydantic import BaseModel
+import re
+from pydantic import BaseModel, validator
 from typing import Optional
+
+from src.enum.tipo_endereco import TipoEndereco
+
 
 
 class Endereco(BaseModel):
     logradouro: str
     numero: int
+    cep: str
     complemento: Optional[str]
     tipo: TipoEndereco
+
+    @validator('numero')
+    def numero_is_valid(cls, v):
+        if v < 0:
+            raise ValueError('numero invalido')
+        return v
+    
+    @validator('cep')
+    def cep_is_valid(cls, v):
+        padrao = re.compile(r"[0-9]{5}\-[0-9]{3}$")
+        valido = padrao.match(v)
+        is_valid = bool(valido)
+        if is_valid != True:
+            raise ValueError('cep invalido')
+        return v
