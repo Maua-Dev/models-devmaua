c7ed4e9d9f4717a5536a30170a4c40439a8c4e79
RENAN SCHEIDT RESCHKE <19.02009-0@maua.br>
2021-07-12 14:49:16 -0300

    formatacao

--- test/models/test_telefone.py
+++ test/models/test_telefone.py
@@ -9,7 +9,10 @@ from src.models.telefone import Telefone
 class Test_Telefone():
     
     def test_create_instance_model(self):
-        tel = Telefone(tipo = TipoTelefone.Privado,numero = '99999-9999',ddd='11',prioridade = 3)
+        tel = Telefone(tipo = TipoTelefone.Privado,
+                       numero = '99999-9999',
+                       ddd='11',
+                       prioridade = 3)
         assert tel.tipo == TipoTelefone.Privado
         assert tel.numero == "99999-9999"
         assert tel.prioridade == 3
