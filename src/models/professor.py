87d8a384cca11a5b70b6ab261aa2fdd0baba9170
Renan Reschke <renanreschke@hotmail.com>
2021-07-12 21:25:07 -0300

    mudancas atributos professor

--- src/models/professor.py
+++ src/models/professor.py
@@ -5,7 +5,7 @@ from src.enum.nome_curso import NomeCurso
 from src.models.disciplina import Disciplina
 from src.models.usuario import Usuario
 
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 
 class Professor(Usuario, BaseModel):
@@ -14,3 +14,20 @@ class Professor(Usuario, BaseModel):
     cursos: list[NomeCurso]
     disciplinas: list[CodigoDisciplina]
     
+    @validator('troncos', check_fields=False)
+    def troncos_is_not_empty(cls, v):
+        if len(v) == 0:
+            raise ValueError('troncos is empty')
+        return v
+    
+    @validator('cursos', check_fields=False)
+    def cursos_is_not_empty(cls, v):
+        if len(v) == 0:
+            raise ValueError('cursos is empty')
+        return v
+    
+    @validator('disciplinas', check_fields=False)
+    def disciplinas_is_not_empty(cls, v):
+        if len(v) == 0:
+            raise ValueError('disciplinas is empty')
+        return v
