2e244278feb04de0c3aa58076d95679180d18850
Renan Reschke <renanreschke@hotmail.com>
2021-07-12 23:40:25 -0300

    atributo disciplina definido pelo codigo, nao mais por uma classe

--- src/models/aula.py
+++ src/models/aula.py
@@ -1,13 +1,14 @@
-from src.models.sala import Sala
-from src.models.professor import Professor
-from src.models.disciplina import Disciplina
-
 from pydantic import BaseModel
 from datetime import datetime, time
 
+from src.enum.codigo_disciplina import CodigoDisciplina
+
+from src.models.sala import Sala
+from src.models.professor import Professor
+
 
 class Aula(BaseModel):
-    disciplina: Disciplina
+    disciplina: CodigoDisciplina
     local: Sala
     horario: datetime
     duracao: time
