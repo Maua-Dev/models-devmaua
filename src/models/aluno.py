9daad39778f868b7649f9efa4cb4677f99f1ab91
Renan Reschke <renanreschke@hotmail.com>
2021-07-12 23:03:23 -0300

    test aluno; validators

--- src/models/aluno.py
+++ src/models/aluno.py
@@ -1,27 +1,35 @@
 from src.enum.periodo import Periodo
+from src.enum.codigo_disciplina import CodigoDisciplina
+from src.enum.nome_curso import NomeCurso
 
 from src.models.usuario import Usuario
-from src.models.curso import Curso
-from src.models.disciplina import Disciplina
 from src.models.ra import RA
 
 from typing import Optional
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, root_validator
 
 
 class Aluno(Usuario, BaseModel):
     ra: RA
-    curso: Curso
+    curso: NomeCurso
     serie: int
-    disciplinas: list[Disciplina]
+    disciplinas: list[CodigoDisciplina]
     periodo: Periodo
-    listaDPs: Optional[list[Disciplina]]
+    listaDPs: Optional[list[CodigoDisciplina]]
     hasDP: bool
 
-    @validator('serie', 'periodo')
+    @root_validator
     def serie_is_valid(cls, v):
         periodo = v.get('periodo')
         serie = v.get('serie')
-        if ((v < 1 and v > 5) and (periodo != Periodo.Diurno)) or ((v < 1 and v > 6) and (periodo != Periodo.Noturno)):
+        if (((serie < 1 or serie > 5) and (periodo == Periodo.Diurno)) or ((serie < 1 or serie > 6) and (periodo == Periodo.Noturno))):
             raise ValueError('Serie invalida')
+        return v
+    
+    @root_validator
+    def hasDP_is_valid(cls, v):
+        hasDP = v.get('hasDP')
+        listaDPs = v.get('listaDPs')
+        if (len(listaDPs) > 0 and hasDP == False) or ((len(listaDPs) == 0 or listaDPs == None) and hasDP == True):
+            raise ValueError('hasDP tem valor invalido')
         return v
