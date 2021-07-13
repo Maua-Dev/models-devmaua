e2bf1dd2f911804b0d8d60b89c3f382106a08d49
Renan Reschke <renanreschke@hotmail.com>
2021-07-12 22:18:18 -0300

    test professor; validators

--- src/models/professor.py
+++ src/models/professor.py
@@ -2,7 +2,6 @@ from src.enum.codigo_disciplina import CodigoDisciplina
 from src.enum.tronco import Tronco
 from src.enum.nome_curso import NomeCurso
 
-from src.models.disciplina import Disciplina
 from src.models.usuario import Usuario
 
 from pydantic import BaseModel, validator
