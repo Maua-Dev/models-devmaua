217a3fa75d8249f02a00923efddd9f7658471110
Renan Reschke <renanreschke@hotmail.com>
2021-07-13 12:00:00 -0300

    validators

--- src/models/projeto.py
+++ src/models/projeto.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, validator, root_validator
 from datetime import time, date, datetime
 from typing import Optional
 
@@ -20,9 +20,25 @@ class Projeto(BaseModel):
     
     @validator('nome')
     def nome_is_not_empty(cls, v):
-        if len(v) == 0 or v == None:
+        if len(v.replace(' ', '')) == 0 or v == None:
             raise ValueError('nome esta vazio')
-        return v.title()
+        return v
+    
+    @validator('participantes', check_fields=False)
+    def participantes_is_not_empty(cls, v):
+        if len(v) == 0:
+            raise ValueError('participantes is empty')
+        return v
+    
+    @root_validator
+    def profOrientador_in_participantes(cls, v):
+        participantes = v.get('participantes')
+        id_orientador = v.get('professorOrientador')
+        for x in participantes:
+            if Roles.Professor in x.roles:
+                if x.ID == id_orientador:
+                    return v
+        raise ValueError('orientador not in participantes')
     
     def getProfessores(self):
         profs = []
@@ -37,3 +53,6 @@ class Projeto(BaseModel):
             if Roles.Aluno in x.roles:
                 alunos.append(x)
         return alunos
+    
+    def _area(self):
+        return self.area.value.title()
