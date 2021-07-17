fecbff0a65059f28c73ffc0a471c15a5d15998c0
Renan Reschke <renanreschke@hotmail.com>
2021-07-17 15:57:03 -0300

    Aluno de Iniciação Científica

---
+++ src/models/aluno_iniciacao.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+from src.enum.roles import Roles
+
+from src.models.aluno import Aluno
+from src.models.projeto import Projeto
+
+class AlunoMonitorDisciplina(Aluno, BaseModel):
+    monitoria: Projeto
+    roles: list[Roles] = [Roles.Aluno, Roles.AlunoIC]
