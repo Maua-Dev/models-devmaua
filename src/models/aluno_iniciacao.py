732009e51067cad7266b813cc5d07502c9aad0be
Renan Reschke <renanreschke@hotmail.com>
2021-07-17 15:59:46 -0300

    Aluno de Iniciação Científica

--- src/models/aluno_iniciacao.py
+++ src/models/aluno_iniciacao.py
@@ -6,5 +6,5 @@ from src.models.aluno import Aluno
 from src.models.projeto import Projeto
 
 class AlunoIniciacao(Aluno, BaseModel):
-    monitoria: Projeto
+    projeto: Projeto
     roles: list[Roles] = [Roles.Aluno, Roles.AlunoIC]
