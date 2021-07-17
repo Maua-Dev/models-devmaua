d8a105a878457644d32149d2f909708f99287589
Renan Reschke <renanreschke@hotmail.com>
2021-07-17 15:59:20 -0300

    Aluno de Iniciação Científica

--- src/models/aluno_iniciacao.py
+++ src/models/aluno_iniciacao.py
@@ -5,6 +5,6 @@ from src.enum.roles import Roles
 from src.models.aluno import Aluno
 from src.models.projeto import Projeto
 
-class AlunoMonitorDisciplina(Aluno, BaseModel):
+class AlunoIniciacao(Aluno, BaseModel):
     monitoria: Projeto
     roles: list[Roles] = [Roles.Aluno, Roles.AlunoIC]
