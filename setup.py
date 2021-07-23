4fddf37bb5738ebccc548db911dc8664973e2ae5
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-07-23 16:41:11 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='models-devmaua',
+    version='',
+    packages=['src', 'src.enum', 'src.main', 'src.models', 'test', 'test.models'],
+    url='https://github.com/Maua-Dev/models-devmaua',
+    license='',
+    author='Dev Comunity Mauá',
+    author_email='',
+    description='Models para uso do projeto do Thiago no Dev Mauá'
+)
