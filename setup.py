f48aa3b797fd1eac7217067c785941f89c2b42f7
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-07-23 17:14:06 -0300

    atualizando setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='devmaua',
-    version='0.0.1',
-    packages=['src', 'src.enum', 'src.main', 'src.models', 'test', 'test.models'],
+    name='models-devmaua',
+    version='0.0.2',
+    packages=['devmaua', 'devmaua.src', 'devmaua.src.enum', 'devmaua.src.main', 'devmaua.src.models', 'devmaua.test',
+              'devmaua.test.models'],
     url='https://github.com/Maua-Dev/models-devmaua',
     license='',
     author='Dev Comunity Mauá',
     author_email='',
-    description='Models para uso do projeto do Thiago no Dev Mauá'
+    description=''
 )
