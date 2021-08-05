0ff0013e7458944f07430fd221f7b14fed4e737a
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2021-08-04 21:11:51 -0300

    atualiza versao do pacote

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='models-devmaua',
-    version='0.0.2',
+    version='0.0.3',
     packages=['devmaua', 'devmaua.src', 'devmaua.src.enum', 'devmaua.src.main', 'devmaua.src.models', 'devmaua.test',
               'devmaua.test.models'],
     url='https://github.com/Maua-Dev/models-devmaua',
